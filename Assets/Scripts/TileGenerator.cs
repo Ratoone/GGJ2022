9a2ba8c32b67c57f19e3144cdbdbb78f4d1c5057
Ratoone <vacariu.paul@gmail.com>
2022-01-29 18:46:45 +0100

    Hourglass figure

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -8,18 +8,20 @@ public class TileGenerator : MonoBehaviour
     private Texture2D levelBitmap;
 
     public int size = 128;
+    public float scale;
+    public string levelName;
     public GameObject referenceTile;
 
     // Start is called before the first frame update
     void Start()
     {
-        levelBitmap = Resources.Load("Levels/Test") as Texture2D;
+        levelBitmap = Resources.Load("Levels/" + levelName) as Texture2D;
 
         tiles = new GameObject[size, size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                GameObject newTile = Instantiate(referenceTile, new Vector3((float)i, 1, (float)j), Quaternion.identity) as GameObject;
-                newTile.transform.localScale = Vector3.one;
+                GameObject newTile = Instantiate(referenceTile, scale * new Vector3((float)i, 0, (float)j), referenceTile.transform.rotation) as GameObject;
+                newTile.transform.localScale *= scale;
                 // newTile.GetComponent<Renderer>().material.SetColor("_Color", Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
                 newTile.GetComponent<Tile>().setInstantColor(levelBitmap.GetPixel(i,j));
                 tiles[i,j] = newTile;
